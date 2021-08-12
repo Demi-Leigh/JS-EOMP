14dd5755f6a2ffffdaf8b528ae2b744bc695ba52
Demi-Leigh <demijefferies@gmail.com>
2021-08-12 16:27:39 +0200

    updates

---
+++ scripts.js
@@ -0,0 +1,6 @@
+let mens = document.querySelector(".navlink.mens")
+let womens = document.querySelector(".navlink.womens")
+let shoes = document.querySelector(".navlink.shoes")
+let accessories = document.querySelector(".navlink.shoes")
+
+
